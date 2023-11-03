3e9ab7b4cdb7a01d69e321932ad162f005661de7
prvnare <arewar.praveen@gmail.com>
2023-11-03 19:39:52 +0900

    Basics of Python

---
+++ LanguageFundamentals.py
@@ -0,0 +1,35 @@
+# **************
+# IDENTIFIERS
+# **************
+
+####
+#
+# a-z, A-Z 0-9 and _ are allowed chars
+# Reserved words are not allowed
+# Case Sensitive language
+# a and A is not the same
+#
+####
+
+name = 'Praveen';   # valid
+
+# 123 = '10' ;      # invalid it should not start with numbers
+
+name123 = 0;        # valid, identifier can contain numbers
+
+# if = 20 ;         # invalid , reserved words not allowed
+
+name_ = 123         # Valid
+
+# name% = 123       # Invalid, % is not allowed as identifier
+
+normalVariable = 'Normal variable declaration';
+_protectedVariable = 'Protected variable declaration in python';
+__privateVariable = 'Private variable declaration';
+__magicVariable__ = 'Language specific variable declaration , wont be used frequently in day to day programs';
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -3,3 +3,9 @@ a = 10;
 b = 20;
 c = 30 if a > b else 40;
 print(c);
+
+print("Hello World");
+print(a+b);
+c = type(a);
+print(c);
+print(type(c));
